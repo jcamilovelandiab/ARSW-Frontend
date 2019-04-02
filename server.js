1406348ffaff5353fa5ac4b8216ed426a7ffbba3
karen <k26duran@gmail.com>
2019-04-01 21:25:42 -0500

    make it better

---
+++ server.js
@@ -0,0 +1,18 @@
+// Instalar el servidor express
+const express = require ('express');
+const path = require ('path');
+
+const app = express ();
+
+// Servir solo los archivos estáticos del directorio dist
+app.use (express.static ('./dist/now-ui-dashboard-angular'));
+
+app.get ('/*', función (req, res)=> {
+    
+res.sendFile (path.join (__ dirname + '/dist/now-ui-dashboard-angular/index.html'));
+});
+
+// Iniciar la aplicación escuchando en el puerto Heroku predeterminado
+app.listen (process.env.PORT || 8080, ()=>{
+	console.log('Server started');
+})
